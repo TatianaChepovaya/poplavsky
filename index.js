d1bf2cd4179640917ebd911e4389dc5fe08974cc
TatianaChepovaya <taniachepovaya@gmail.com>
2023-03-09 10:13:09 +0200

    .

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-  $('.your-class').slick({
+  $('.biography__slider').slick({
     slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
+})
+$(document).ready(function () {
+  $('.figures__slider').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
