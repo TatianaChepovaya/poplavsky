70d0ac710e20a32ec80af3de23281b965da5531c
TatianaChepovaya <taniacheport@gmail.com>
2023-03-26 11:33:40 +0300

    ,

--- index.js
+++ index.js
@@ -44,6 +44,65 @@ $(document).ready(function () {
     autoplaySpeed: 2000,
   })
 })
+$(document).ready(function () {
+  $('.books__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  })
+})
+$(document).ready(function () {
+  $('.project__article-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
+})
+
+$(document).ready(function () {
+  $('.project__article-slider-big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+  })
+})
 
 $(document).ready(function ($) {
   if ($(window).width() < 1024) {
